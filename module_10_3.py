4bb15413e6aa607b87b3be9178632b163b1d3e88
anti-anime228 <qwerty9228@bk.ru>
2024-08-24 12:43:32 +0300

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -9,31 +9,30 @@ class Bank:
         self.lock = threading.Lock()
 
     def deposit(self):
-        transactions_plus = 10
-        random_number1 = random.randint(50, 500)
-        with self.lock:
-            while transactions_plus > 0:
-                if self.balance >= 500 and self.lock.locked():
-                    self.lock.release()
-                self.balance += random_number1
-            print(f"Пополнение: {random_number1}. Баланс: {self.balance}")
+        transactions_plus = 100
+        while transactions_plus > 0:
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+                time.sleep(0.1)
+            self.balance += random.randint(50, 500)
+            print(f"Пополнение: {random.randint(50, 500)}. Баланс: {self.balance}")
             transactions_plus -= 1
             time.sleep(0.1)
 
     def take(self):
-        transactions_minus = 10
-        random_number2 = random.randint(50, 500)
-        print(f"Запрос на {random_number2}")
-        with self.lock:
-            while transactions_minus > 0:
-                if random_number2 <= self.balance:
-                    self.balance -= random_number2
-                    print(f"Снятие: {random_number2}. Баланс: {self.balance}")
-                    transactions_minus -= 1
-                else:
-                    print("Запрос отклонён, недостаточно средств")
-                    if not self.lock.locked():
-                        self.lock.acquire()
+        transactions_minus = 100
+        print(f"Запрос на {random.randint(50, 500)}")
+        while transactions_minus > 0:
+            if random.randint(50, 500) <= self.balance:
+                self.balance -= random.randint(50, 500)
+                print(f"Снятие: {random.randint(50, 500)}. Баланс: {self.balance}")
+                transactions_minus -= 1
+                time.sleep(0.1)
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                if not self.lock.locked():
+                    self.lock.acquire()
+                transactions_minus -= 1
                 time.sleep(0.1)
 
 
@@ -48,5 +47,3 @@ th1.join()
 th2.join()
 
 print(f'Итоговый баланс: {bk.balance}')
-print(f'Итоговый баланс: {bk.balance}')
-
